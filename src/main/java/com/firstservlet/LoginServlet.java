3b00cd14e6d26f2c389a0f7bae3d192c90623b01
snehapal2012 <snehapal2012@gmail.com>
2022-08-27 12:38:50 +0530

    Modified: check for valid password

--- src/main/java/com/firstservlet/LoginServlet.java
+++ src/main/java/com/firstservlet/LoginServlet.java
@@ -23,9 +23,11 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
     String user=request.getParameter("user");
     String pwd=request.getParameter("pwd");
+
     String userID=getServletConfig().getInitParameter("user");
     String password= getServletConfig().getInitParameter("password");
-    if(userID.equals(user) && password.equals(pwd) && (userID.matches("^[A-Z]{1}[a-z]{2,}$"))){
+
+    if(userID.equals(user) && password.equals(pwd) && (userID.matches("^[A-Z]{1}[a-z]{2,}$")) && (password.matches("^(?=.*[A-z])(?=.*[0-9])[-+_!@#$%^&*.,?0-9A-z]{8,}$"))){
         request.setAttribute("user",user);
         request.getRequestDispatcher("LoginSuccess.jsp").forward(request,response);
     }else{
@@ -48,7 +50,7 @@ public class LoginServlet extends HttpServlet {
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request,response);
         }
         else{
-            RequestDispatcher rd= getServletContext().getRequestDispatcher("/Login.html");
+            RequestDispatcher rd= getServletContext().getRequestDispatcher("/login.html");
             PrintWriter out=response.getWriter();
             out.println("<font color=red>Either user name or password is wrong.</font>");
             rd.include(request,response);
